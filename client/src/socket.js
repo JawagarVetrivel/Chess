ff1392061cb09129ca064760b209d0f5178c1804
JawagarVetrivel <jawagarvetri@gmail.com>
2026-01-12 18:49:16 +0530

    Fix socket connectivity issues: Use env var for URL with local fallback

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,13 @@
 import { io } from "socket.io-client";
 
 // In production, this URL should probably be an env var
-const URL = "https://chess-server-fqa7.onrender.com";
+const URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
+console.log("Connecting to socket at:", URL);
 
 export const socket = io(URL, {
-    autoConnect: false
+    autoConnect: false,
+});
+
+socket.on("connect_error", (err) => {
+    console.error("Socket connection error:", err);
 });
